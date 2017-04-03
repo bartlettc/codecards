04585928d02e385bdc95b83fd42d8ccf2f09622f
Charlie <cbartlett@gmail.com>
2017-04-03 10:04:17 +0100

    Clean up the routes file

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-
-
-    return view('home', ['code' => '<?php 
-    echo "hello world"; 
-    function Hello() {
-        return count("blah");
-    }' ]);
+    return view('home');
 });
 
 
 Route::post('getimg', 'CardController@create');
 Route::get('/{id}', 'CardController@display');
 
-//
-//Route::group(['domain' => '{account}.codecards'], function () {
-//    Route::get('/{id}', function ($account, $id) {
-//        echo $id.' '.$account;
-//    });
-//
-//
-//});
